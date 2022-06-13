65f0c979a3a248b5db45e2d63f0a5f0351850fea
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-13 14:45:52 +0200

    rpy

--- Navio2/Python/main.py
+++ Navio2/Python/main.py
@@ -127,9 +127,12 @@ if __name__ == "__main__" :
 				while True:
 					r_p_y=[b_state.roll,b_state.pitch,b_state.yaw]
 					print(r_p_y)
-					data=struct.pack('3f',*r_p_y)
-					conn.sendall(data)
-					time.sleep(0.2)
+					try :
+						data=struct.pack('3f',*r_p_y)
+						conn.sendall(data)
+						time.sleep(0.2)
+					except :
+						continue
 		# rasp.pos=vector(b_state.x,b_state.y,0)
 		# rasp.axis=vector(b_state.roll,b_state.pitch,b_state.yaw)
 		# rasp=box(canvas=scene,pos=vector(0,0,0),axis=rasp.axis,length=4,height=1,width=2,color=color.green)
