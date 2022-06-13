cef28f0b0e8464ca5f56585b8c674779cedcc7a0
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-13 17:54:24 +0200

    boat

--- Navio2/Python/main.py
+++ Navio2/Python/main.py
@@ -120,6 +120,7 @@ if __name__ == "__main__" :
 	#Données :
 	while not b_state.end :
 		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 			s.bind((HOST, PORT))
 			s.listen()
 			conn, addr = s.accept()
--- Navio2/Python/visu.py
+++ Navio2/Python/visu.py
@@ -8,7 +8,13 @@ PORT = 65432  # The port used by the server
 if __name__ == "__main__" :
 	# scene=canvas(width=200,height=200,title="Scene",caption='Yo')
 	# # earth=box(pos=vector(0,-1,0),length=1000,height=1,width=1000,color=color.blue)
-	rasp=box(pos=vector(0,0,0),length=2,height=1,width=3,color=color.red)
+	rasp=box(pos=vector(0,0,0),length=4,height=1,width=2,color=color.red)
+	mast=cylinder(pos=vector(0,0.5,0),length=3,axis=vector(0,1,0),radius=0.2,color=color.red)
+	front_b=pyramid(pos=vector(2,0,0),axis=vector(1,0,0),size=vector(2,1,2),color=color.red)
+	rudder=box(pos=vector(-2,-0.5,0),axis=vector(1,0,0),size=vector(1,1.2,0.5))
+	sail=triangle(v0=vertex(pos=vec(-0.2,3.5,0)),v1=vertex(pos=vec(-0.2,1,0)),v2=vertex(pos=vec(-2,1,0)))
+	mast_sail=compound([mast,sail])
+	s_boat=compound([rasp,mast_sail,front_b,rudder])
 	# arr_b=pyramid(pos=rasp.pos+vector(2,0,0),color=color.red,length=1,height=1,width=2)
 	x_axis=arrow(pos=vector(0,0,0),axis=vector(1,0,0),color=color.blue)
 	y_axis=arrow(pos=vector(0,0,0),axis=vector(0,0,1),color=color.green)
@@ -24,7 +30,9 @@ if __name__ == "__main__" :
 				continue
 			x,y,z=rpy
 			rasp.pos=vector(0,0,0)
-			rasp.rotate(angle=-radians(old_x-x),axis=vector(cos(radians(z)),0,-sin(radians(z))),origin=vector(0,0,0))
-			rasp.rotate(angle=-radians(old_y-y),axis=vector(sin(radians(0)),0,cos(radians(0))),origin=vector(0,0,0))
-			rasp.rotate(angle=radians(old_z-z),axis=vector(0,-1,0),origin=vector(0,0,0))
+			s_boat.rotate(angle=-radians(old_x-x),axis=vector(cos(radians(z)),0,-sin(radians(z))),origin=vector(0,0,0))
+			s_boat.rotate(angle=-radians(old_y-y),axis=vector(sin(radians(z)),0,cos(radians(z))),origin=vector(0,0,0))
+			s_boat.rotate(angle=radians(old_z-z),axis=vector(0,-1,0),origin=vector(0,0,0))
+			# mast.axis=vector(-rasp.axis.y,rasp.axis.z,rasp.axis.x)
 			old_x,old_y,old_z=x,y,z
+			print(s_boat.axis)
