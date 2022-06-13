1a4842d5faf25f8c1deb7ef84f8036ec0dfef9fe
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-13 11:47:34 +0200

    main et visu

--- Navio2/Python/visu.py
+++ Navio2/Python/visu.py
@@ -7,12 +7,14 @@ PORT = 65432  # The port used by the server
 if __name__ == "__main__" :
 	# scene=canvas(width=200,height=200,title="Scene",caption='Yo')
 	# # earth=box(pos=vector(0,-1,0),length=1000,height=1,width=1000,color=color.blue)
-	# rasp=box(pos=vector(0,0,0),axis=vector(0,0,0),length=4,height=1,width=2,color=color.red)
-	# # arr_b=pyramid(pos=rasp.pos+vector(2,0,0),color=color.red,length=1,height=1,width=2)
+	rasp=box(pos=vector(0,0,0),axis=vector(0,0,0),length=2,height=1,width=3,color=color.red)
+	# arr_b=pyramid(pos=rasp.pos+vector(2,0,0),color=color.red,length=1,height=1,width=2)
 
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	    s.connect((HOST, PORT))
 	    while True :
 		    data = s.recv(1024)
-		    loc=struct.unpack('2f',data)
-		    print(loc)
+		    rpy=struct.unpack('3f',data)
+		    print(rpy)
+		    rasp.pos=vector(0,0,0)
+		    rasp.axis=norm(vector(rpy[0],rpy[1],rpy[2]))
