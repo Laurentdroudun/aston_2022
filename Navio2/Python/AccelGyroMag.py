acafb115e13fd63ba46bea27b6bff8cd6634cd6a
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-16 13:36:21 +0200

    calib

--- Navio2/Python/AccelGyroMag.py
+++ Navio2/Python/AccelGyroMag.py
@@ -24,7 +24,7 @@ def acc_gyr_mag(imu):
 	offset_acc_y=-0.5670094354461468
 	offset_acc_z=-0.3401383226787864
 	offset_mag_x=-7.959210618259436
-	offset_mag_y=-15.521659859117926
+	offset_mag_y=-21.521659859117926
 	offset_mag_z=-11.650658574455132
 	m9a[0]-=offset_acc_x
 	m9a[1]-=offset_acc_y
@@ -55,8 +55,8 @@ def pitch(imu) :
 
 def rpy(imu) :
 	m9a, m9g, m9m = acc_gyr_mag(imu)
-	roll=180*atan2(m9a[0],sqrt(m9a[0]**2+m9a[2]**2))/pi
-	pitch=180*atan2(m9a[1],sqrt(m9a[1]**2+m9a[2]**2))/pi
+	roll=2*180*atan2(m9a[0],sqrt(m9a[0]**2+m9a[2]**2))/pi
+	pitch=2*180*atan2(m9a[1],sqrt(m9a[1]**2+m9a[2]**2))/pi
 	yaw=atan2(m9m[1],m9m[0])*180/pi
 	return roll,pitch,yaw
 
