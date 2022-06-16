2048905b7ee68683677c6f9b945bf90851dffd5d
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-16 16:04:53 +0200

    filter imu

--- Navio2/Python/AccelGyroMag.py
+++ Navio2/Python/AccelGyroMag.py
@@ -20,19 +20,19 @@ def init_imu_mpu() :
 
 def acc_gyr_mag(imu):
 	m9a, m9g, m9m = imu.getMotion9()
-	offset_acc_x=-0.018269624314355457
-	offset_acc_y=-0.5670094354461468
-	offset_acc_z=-0.3401383226787864
-	offset_mag_x=-7.959210618259436
-	offset_mag_y=-21.521659859117926
-	offset_mag_z=-11.650658574455132
-	m9a[0]-=offset_acc_x
-	m9a[1]-=offset_acc_y
-	m9a[2]-=offset_acc_z
-	m9m[0]-=offset_mag_x
-	m9m[1]-=offset_mag_y
-	m9m[2]-=offset_mag_z
-	return m9a*1/9.81,m9g,m9m
+	offset_acc=[]
+	offset_mag=[]
+	offset_acc.append(-0.018269624314355457)
+	offset_acc.append(-0.5670094354461468)
+	offset_acc.append(-0.3401383226787864)
+	offset_mag.append(-7.959210618259436)
+	offset_mag.append(-21.521659859117926)
+	offset_mag.append(-11.650658574455132)
+	for k in range(3) :
+		m9a[k]-=offset_acc[k]
+		m9a[k]/=9.81
+		m9m[k]-=offset_mag[k]
+	return 1/9.81*eye(3)@m9a,m9g,m9m
 
 
 def yaww(imu) :
