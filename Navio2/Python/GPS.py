63374292060974d3df872d67dc27f2e3c37e0c5b
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-20 11:40:27 +0200

    vitesse

--- Navio2/Python/GPS.py
+++ Navio2/Python/GPS.py
@@ -47,8 +47,9 @@ def gps(ubl) :
             ubl = navio2.ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
         # print(empty)
     if msg.name() == "NAV_POSLLH":
-        lon_lat,vit = [int(str(msg).split(",")[1][11:]),int(str(msg).split(",")[2][10:])],str(msg)
-        return lon_lat,vit                       #Returns the longitude and the latitude 
+        # lon_lat,vit = [int(str(msg).split(",")[1][11:]),int(str(msg).split(",")[2][10:])],str(msg)
+        vit=str(ms)
+        return vit                       #Returns the longitude and the latitude 
     if msg.name() == "NAV_STATUS":
         outstr = str(msg).split(",")[1:2]
         outstr = "".join(outstr)
@@ -56,5 +57,5 @@ def gps(ubl) :
 if __name__=="__main__" :
     while True :
         ubl=init_gps()
-        lon_lat,vit=gps(ubl)
-        print("lon_lat : {}, vitesse : {}".format(lon_lat,vit))
+        vit=gps(ubl)
+        print("lon_lat : #, vitesse : {}".format(vit))
