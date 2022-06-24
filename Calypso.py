e4299baf3df57a9f1fe72db4b2bb1b51eab31fe4
Laurent Droudun <laurent.droudun@gmail.com>
2022-06-24 12:41:51 +0200

    Calypso & Main

--- Calypso.py
+++ Calypso.py
@@ -147,7 +147,7 @@ def wind(dev) :
         dev.waitForNotifications(1.0)
         return(dev.delegate.getWind())
     except:
-        continue
+        print("Bug")
      # if(dev.delegate.getBattery() != 0):
      #     print(dev.delegate.getBattery())
 
